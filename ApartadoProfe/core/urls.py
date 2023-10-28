eaefc826f200b85d0a0063e388fc7578d5c8a659
lorquita <florcadiaz@gmail.com>
2023-10-27 23:24:29 -0300

    correcion

--- ApartadoProfe/core/urls.py
+++ ApartadoProfe/core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', inicio, name='inicio-sesion'),
     path('home', home, name='home'),
     path('asistencia', asistencia, name="asistencia"),
-    path('QR/',QR,name='QR'),
-    path('api/generate-qr/<str:data>/', generate_qr_code, name='generate_qr_code'),
+    path('PagQR',PagQR,name='PagQR'),
+    
 ]
